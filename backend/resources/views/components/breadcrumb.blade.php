268dfcabb6622dedec96dbe67503fd8bf5d60003
yudaahscwlth <raflesyudaa@gmail.com>
2025-10-05 14:59:18 +0700

    Menghapus File proyek Sem 6

--- backend/resources/views/components/breadcrumb.blade.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="">Home</a></li>
-        <li class="breadcrumb-item active text-capitalize">
-            {{ ucwords(str_replace('/', ' / ', Request::path())) }}
-        </li>
-    </ol>
-</nav>
