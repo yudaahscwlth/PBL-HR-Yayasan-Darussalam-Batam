71a68326363e63740c4d50fca28b54779232672a
yudaahscwlth <raflesyudaa@google.com>
2025-10-05 14:59:18 +0700

    Menghapus File proyek Sem 6

--- backend/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Resources\UserResource;
-use Illuminate\Support\Facades\Route;
-use App\Models\User;
-
-Route::get('/users', function () {
-    return UserResource::collection(User::all());
-});
