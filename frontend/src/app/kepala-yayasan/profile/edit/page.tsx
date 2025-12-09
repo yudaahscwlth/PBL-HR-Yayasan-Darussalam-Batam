ff5a60a1d1a7f986c6b9512d75896220ca4d3e1a
yudaahscwlth <raflesyudaa@gmail.com>
2025-12-09 16:27:26 +0700

    Add Profile Page Kepala Yayasan

---
+++ frontend/src/app/kepala-yayasan/profile/edit/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import ProfileEdit from "@/components/ProfileEdit";
+
+export default function KepalaYayasanProfileEdit() {
+  return <ProfileEdit allowedRoles={["kepala yayasan"]} />;
+}
+
+
